d61b473b7469f46a59456420c4a983eb681e146c
Fabio Ricali <ricalifabio@gmail.com>
2018-06-24 22:14:23 +0200

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import cmp from './src/main'
 
 // expose component to global scope
 if (typeof window !== 'undefined' && window.Doz) {
-    Doz.component('your-component-tag', cmp)
+    Doz.component('doz-searchfield', cmp)
 }
 
 export default cmp
--- src/main.js
+++ src/main.js
@@ -3,31 +3,77 @@ import './style.css'
 export default {
 
     props: {
-        name: 'DOZ'
+        name: '',
+        value: '',
+        showButton: false,
+        buttonLabel: 'Search',
+        placeholder: 'Search here...',
+        buttonClass: '',
+        inputClass: '',
+        inputId: 'search-field-id',
+        showClearButton: false,
+        _showCross: false
     },
 
     template() {
         return `
-            <div class="my-component">
-                hello ${this.props.name}
-            </div>
+            <input
+                placeholder="${this.props.placeholder}"
+                class="${this.props.inputClass}"
+                type="text"
+                name="${this.props.name}"
+                value="${this.props.value}"
+                oninput="this.$onInput()"
+                onkeydown="this.$onKeyDown()"
+                d-bind="value"
+                id="${this.props.inputId}"
+            >
+            <button 
+                class="${this.props.buttonClass}" 
+                style="display: ${this.props._showCross ? 'inline' : 'none'}" 
+                onclick="this.$clear()"
+            >X</button>
+            
+            <button 
+                class="${this.props.buttonClass}" 
+                style="display: ${this.props.showButton ? 'inline' : 'none'}" 
+                onclick="this.$search()"
+            >${this.props.buttonLabel}</button>
         `
     },
 
     onCreate() {
-
+        this.props._showCross = this.$canCross();
     },
 
-    onMount() {
+    $isSearching: false,
 
+    $canCross() {
+        return this.props.value.length > 0 && this.props.showClearButton;
     },
 
-    onUpdate() {
-
+    $onInput() {
+        this.props._showCross = this.$canCross();
+        if (this.props.value.length === 0 && this.$isSearching)
+            this.$clear();
     },
 
-    onDestroy() {
+    $onKeyDown(e) {
+        if (e.keyCode === 13) {
+            this.$search();
+        }
+    },
 
+    $search() {
+        this.emit('search', {search: this.props.value});
+        this.$isSearching = true;
+    },
+
+    $clear() {
+        this.props.value = '';
+        this.props._showCross = false;
+        this.emit('clear', {search: ''});
+        this.$isSearching = false;
     }
 
 };
